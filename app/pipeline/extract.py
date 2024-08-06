954a8406a302a2c5804c30ebfa0fc2c917604a80
Alexandre Medeiros <alexandremedeiros@MacBook-Air-de-Alexandre.local>
2024-08-05 22:00:20 -0300

    Junta todos os arquivos excel em um DataFrame

---
+++ app/pipeline/extract.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os #manipular arquivos e pastas
+import glob #listar arquivos
+from typing import List
+
+def extract_from_excel(path: str) -> List[pd.DataFrame]:
+    """
+    função para ler os arquivos de
+    uma pasta data/input e retornar uma lista de dataframes
+
+    args: input_path (str): caminho da pasta com os arquivos
+
+    return: lista de dataframes
+    """
+    all_files = glob.glob(os.path.join(path, "*.xlsx"))
+
+    data_frame_list = []
+    for file in all_files:
+        data = pd.read_excel(file)
+        data_frame_list.append(data)
+
+    return data_frame_list
+
+
+# if __name__ == "__main__":
+#     data_frame_list = extract_from_excel("data/input")
+#     print(data_frame_list)
