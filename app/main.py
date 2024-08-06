7377ce164229d0ea9401ce2a7083708b9e4a6727
Alexandre Medeiros <alexandremedeiros@MacBook-Air-de-Alexandre.local>
2024-08-05 22:42:53 -0300

    transform and load

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 from pipeline.extract import extract_from_excel
+from pipeline.transform import concat_data_frames
+from pipeline.load import load_excel
 
-data_frame_list = extract_from_excel("data/input")
-print(data_frame_list)
+
+if __name__ == "__main__":
+    data_frame_list = extract_from_excel("data/input")
+    data_frame = concat_data_frames(data_frame_list)
+    load_excel(data_frame, "data/output", "output")
+    print(data_frame_list)
